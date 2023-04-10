7dd290446b8deb71099d1aced6c7fa596adce0c3
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2023-04-09 21:27:43 -0700

    Add unstyled color slider
    - Add rc-slider to control shade of palette

--- src/palette/Palette.tsx
+++ src/palette/Palette.tsx
@@ -1,3 +1,6 @@
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
+import { useState } from 'react'
 import ColorBox from '../colorBox/ColorBox'
 import { Palette } from '../utils/colorHelper'
 import './Palette.css'
@@ -7,14 +10,26 @@ interface PaletteProps {
 }
 
 export default function Palette(props: PaletteProps) {
+    const [level, setLevel] = useState(500)
     const { palette } = props
 
-    const colorBoxes = palette.colors[300].map((color) => (
+    const colorBoxes = palette.colors[level].map((color) => (
         <ColorBox background={color.hex} name={color.name} />
     ))
     return (
         <div className={'Palette'}>
+            <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onChange={changeLevel}
+            />
             <div className={'Palette-colors'}>{colorBoxes}</div>
         </div>
     )
+
+    function changeLevel(newLevel: number | number[]) {
+        if (!Array.isArray(newLevel)) setLevel(newLevel)
+    }
 }
