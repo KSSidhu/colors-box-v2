66dfd6f2e12975f1a70c133d79a6b2fd447a2dab
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2023-04-09 19:54:56 -0700

    Add color helper util

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
 import Palette from './palette/Palette'
 import seedColor from './seed/seedColor'
+import { generatePalette } from './utils/colorHelper'
 
 function App() {
+    console.log(generatePalette(seedColor[4]))
     return (
         <div className="App">
             <Palette palette={seedColor[4]} />
