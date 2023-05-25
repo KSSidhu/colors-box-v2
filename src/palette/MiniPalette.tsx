b13995715ade5d7a7d077cca1823e2001f42bed9
Kirat Sidhu <kirat.sidhu.sw@gmail.com>
2023-05-24 21:55:00 -0700

    Add styled MiniPalette component
    - Style PaletteList and MiniPalette

--- src/palette/MiniPalette.tsx
+++ src/palette/MiniPalette.tsx
@@ -1,18 +1,75 @@
 import { makeStyles } from '@mui/styles'
+import { BasePalette } from '../utils/colorHelper'
 
-function MiniPalette() {
+type MiniPaletteProps = BasePalette
+
+function MiniPalette(props: MiniPaletteProps) {
+    const { paletteName, emoji, colors } = props
     const classes = useStyles()
+
+    const minicolorBoxes = colors.map((color) => (
+        <div
+            key={`${color.name}-${color.color}`}
+            className={classes.miniBox}
+            style={{ backgroundColor: color.color }}
+        />
+    ))
+
     return (
-        <div className={classes.main}>
-            <h1>{'Mini Palette'}</h1>
+        <div className={classes.root}>
+            <div className={classes.colours}>
+                {/* Mini colour boxes*/}
+                {minicolorBoxes}
+            </div>
+            <h5 className={classes.title}>
+                {paletteName}
+                <span className={classes.emoji}>{emoji}</span>
+            </h5>
         </div>
     )
 }
 
 const useStyles = makeStyles({
-    main: {
-        backgrounColor: 'purple',
-        border: '3px solid teal',
+    root: {
+        backgroundColor: 'white',
+        border: '1px solid black',
+        borderRadius: '5px',
+        padding: '0.5rem',
+        position: 'relative',
+        overflow: 'hidden',
+        '&:hover': {
+            cursor: 'pointer',
+        },
+    },
+    colours: {
+        backgroundColor: '#dae1e4',
+        height: '150px',
+        width: '100%',
+        borderRadius: '5px',
+        overflow: 'hidden',
+    },
+    title: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        margin: '0',
+        color: 'black',
+        paddingTop: '0.5rem',
+        paddingBottom: '0.75rem',
+        fontSize: '1rem',
+        position: 'relative',
+    },
+    emoji: {
+        marginLeft: '0.5rem',
+        fontSize: '1.5rem',
+    },
+    miniBox: {
+        height: '25%',
+        width: '20%',
+        display: 'inline-block',
+        margin: '0 auto',
+        position: 'relative',
+        marginBottom: '-3.5px',
     },
 })
 
--- src/palette/PaletteList.tsx
+++ src/palette/PaletteList.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { makeStyles } from '@mui/styles'
 import { BasePalette } from '../utils/colorHelper'
 import MiniPalette from './MiniPalette'
 
@@ -7,19 +7,51 @@ interface PaletteListProps {
 }
 
 function PaletteList({ palettes }: PaletteListProps) {
+    const classes = useStyles()
     return (
-        <div>
-            <MiniPalette />
-            <h1>{'React Colors'}</h1>
-            {palettes.map((palette) => (
-                <p>
-                    <Link to={`/palette/${palette.id}`}>
-                        {palette.paletteName}
-                    </Link>
-                </p>
-            ))}
+        <div className={classes.root}>
+            <div className={classes.container}>
+                <nav className={classes.nav}>
+                    <h1>{'React Colors'}</h1>
+                </nav>
+                <div className={classes.palettes}>
+                    {palettes.map((palette) => (
+                        <MiniPalette {...palette} />
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
+
+const useStyles = makeStyles({
+    root: {
+        backgroundColor: 'blue',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+    },
+    container: {
+        width: '50%',
+        display: 'flex',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
+        flexWrap: 'wrap',
+    },
+    nav: {
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        color: 'white',
+    },
+    palettes: {
+        boxSizing: 'border-box',
+        width: '100%',
+        display: 'grid',
+        gridTemplateColumns: 'repeat(3, 30%)',
+        gridGap: '5%',
+    },
+})
 
 export default PaletteList
